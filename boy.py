406078d5364d0d247e65e559524a757d86fafbb0
Yang hyeon bin <kirby47naver.com@tukorea.ac.kr>
2025-10-16 15:11:33 +0900

    왜 스페이스바가 안댐

--- boy.py
+++ boy.py
@@ -3,20 +3,85 @@ from sdl2 import SDL_KEYDOWN, SDLK_SPACE, SDLK_RIGHT, SDL_KEYUP, SDLK_LEFT
 
 from state_machine import StateMachine
 
+# 이벤트 체크 함수
+
+def space_down(e):
+    return e[0]=='INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_SPACE
+
+def time_out(e):
+    return e[0]=='TIME_OUT'
+
+def right_down(e):
+    return e[0]=='INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_RIGHT
+def right_up(e):
+    return e[0]=='INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_RIGHT
+def left_down(e):
+    return e[0]=='INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_LEFT
+def left_up(e):
+    return e[0]=='INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_LEFT
+
+class Run:
+
+    def __init__(self, boy):
+        self.boy = boy
+
+    def enter(self,e):
+        if right_down(e) or left_up(e):
+            self.boy.dir = self.boy.face_dir=1
+        elif left_down(e) or right_up(e):
+            self.boy.dir = self.boy.face_dir=-1
+
+    def exit(self,e):
+        pass
+
+    def do(self):
+        self.boy.frame = (self.boy.frame + 1) % 8
+        self.boy.x += self.boy.dir*5
+
+
+    def draw(self):
+        if self.boy.face_dir == 1: # right
+            self.boy.image.clip_draw(self.boy.frame * 100, 100, 100, 100, self.boy.x, self.boy.y)
+        else: # face_dir == -1: # left
+            self.boy.image.clip_draw(self.boy.frame * 100, 0, 100, 100, self.boy.x, self.boy.y)
+
+class Sleep:
+
+    def __init__(self, boy):
+        self.boy = boy
+
+    def enter(self,e):
+        self.boy.dir = 0
+
+    def exit(self,e):
+        pass
+
+    def do(self):
+        self.boy.frame = (self.boy.frame + 1) % 8
+
+    def draw(self):
+        if self.boy.face_dir == 1: # right
+            self.boy.image.clip_composite_draw(self.boy.frame * 100, 300, 100, 100, 3.141592/2,'',self.boy.x-25, self.boy.y-25,100,100)
+        else: # face_dir == -1: # left
+            self.boy.image.clip_composite_draw(self.boy.frame * 100, 200, 100, 100, -3.141592/2,'',self.boy.x+25, self.boy.y-25,100,100)
+
 
 class Idle:
 
     def __init__(self, boy):
         self.boy = boy
+        self.boy_wait_start_time = get_time()
 
-    def enter(self):
+    def enter(self,e):
         self.boy.dir = 0
 
-    def exit(self):
+    def exit(self,e):
         pass
 
     def do(self):
         self.boy.frame = (self.boy.frame + 1) % 8
+        if get_time() - self.boy_wait_start_time> 2.0:
+            self.boy.state_machine.handle_state_event(('TIME_OUT',None))
 
     def draw(self):
         if self.boy.face_dir == 1: # right
@@ -34,7 +99,16 @@ class Boy:
         self.image = load_image('animation_sheet.png')
 
         self.IDLE = Idle(self)
-        self.state_machine = StateMachine(self.IDLE)
+        self.SLEEP = Sleep(self)
+        self.RUN = Run(self)
+        self.state_machine = StateMachine(
+            self.IDLE,#시작 상태
+            {
+                self.SLEEP: {space_down: self.IDLE},
+                self.IDLE: {right_up:self.RUN,left_up:self.RUN,right_down:self.RUN,left_down:self.RUN,time_out:self.SLEEP},
+                self.RUN: {right_down:self.RUN,left_down:self.RUN,right_up:self.IDLE,left_up:self.IDLE}
+            }
+        )
 
     def update(self):
         self.state_machine.update()
@@ -42,3 +116,10 @@ class Boy:
 
     def draw(self):
         self.state_machine.draw()
+
+    def handle_event(self, event):
+        self.state_machine.handle_state_event(('INPUT',event))
+
+
+
+        pass
